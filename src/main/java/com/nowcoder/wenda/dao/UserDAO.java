8d7ec280b341d168f18da7993acbed94bb87076f
sguo0801 <sguo_17@sjtu.edu.cn>
2019-03-15 00:53:24 +0800

    解决了initdatabasetests不能运行的问题,完成了数据库的配置

--- src/main/java/com/nowcoder/wenda/dao/UserDAO.java
+++ src/main/java/com/nowcoder/wenda/dao/UserDAO.java
@@ -14,7 +14,7 @@ public interface UserDAO {
     @Insert({"insert into ", TABLE_NAME, " (", INSERT_FIELDS, ") values(#{name}, #{password}, #{salt}, #{headUrl})"})
     int addUser(User user);
 
-    @Select({"select ", SELECT_FIELDS, " from ", TABLE_NAME, " where id=#{id}"})
+    @Select({" select ", SELECT_FIELDS, " from ", TABLE_NAME, " where id=#{id}"})
     User selectById(int id);
 
     @Select({" select ", SELECT_FIELDS, " from ", TABLE_NAME, " where name=#{name}"})
