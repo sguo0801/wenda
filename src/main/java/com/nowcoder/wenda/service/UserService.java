e0b7684997a1e6344e3320afa4aa5bddf8189189
sguo0801 <sguo_17@sjtu.edu.cn>
2019-03-15 17:59:03 +0800

    第三节课数据库连接结束

---
+++ src/main/java/com/nowcoder/wenda/service/UserService.java
@@ -0,0 +1,23 @@
+package com.nowcoder.wenda.service;
+
+import com.nowcoder.wenda.dao.UserDAO;
+import com.nowcoder.wenda.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by nowcoder on 2016/7/2.
+ */
+@Service
+public class UserService {
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+    @Autowired
+    private UserDAO userDAO;
+
+    public User getUser(int id) {
+        return userDAO.selectById(id);
+    }
+
+}
