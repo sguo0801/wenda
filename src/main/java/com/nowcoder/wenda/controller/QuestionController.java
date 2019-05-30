0164f8278bca95a73e3b311239847009a233852e
sguo0801 <sguo_17@sjtu.edu.cn>
2019-05-30 11:12:58 +0800

    自写文本内容敏感词过滤

--- src/main/java/com/nowcoder/wenda/controller/QuestionController.java
+++ src/main/java/com/nowcoder/wenda/controller/QuestionController.java
@@ -39,7 +39,7 @@ public class QuestionController {
     public String questionDetail(Model model, @PathVariable("qid") int qid) {
         Question question = questionService.getById(qid);
         model.addAttribute("question", question);
-        //model.addAttribute("user", userService.getUser(question.getUserId()));
+        model.addAttribute("user", userService.getUser(question.getUserId()));
 
         List<Comment> commentList = commentService.getCommentsByEntity(qid, EntityType.ENTITY_QUESTION);
         List<ViewObject> vos = new ArrayList<>();
